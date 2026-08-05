b88ba66ac8a9c252591dd813565766dc9e024b14
1788809 <kevingithua372@gmail.com>
2026-08-05 16:55:58 +0300

    Initial Calculator Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 20;
+        int b = 10;
+
+        Console.WriteLine("Addition: " + (a + b));
+        Console.WriteLine("Subtraction: " + (a - b));
+        Console.WriteLine("Multiplication: " + (a * b));
+        Console.WriteLine("Division: " + (a / b));
+
+        Console.WriteLine("Program Complete");
+    }
+}
